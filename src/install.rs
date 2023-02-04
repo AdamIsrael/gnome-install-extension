0bff6f80ca9821c90268a39618c6c94fc83e19cd
Adam Israel <adam@adamisrael.com>
2023-02-03 21:33:00 -0500

    Cleanup & Error handling
    Remove some dead code/comments, and start improving the error handling.

--- src/install.rs
+++ src/install.rs
@@ -1,9 +1,10 @@
 use crate::gnome;
 use crate::search;
 
-use std::io::{copy, Cursor};
+use std::error::Error;
 use std::fs;
 use std::fs::File;
+use std::io::{copy, Cursor};
 use std::path::{Path, PathBuf};
 use std::process;
 
@@ -12,12 +13,8 @@ use regex::Regex;
 use reqwest;
 
 /// Download a url to a temporary file
-// pub fn download(url: String) -> Result<(), Box<dyn std::error::Error>> {
 pub fn download(url: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
-    // let tmp_dir = Builder::new().prefix("example").tempdir()?;
-
-    let tmp_dir = std::env::temp_dir()
-        .join(format!("gnome-extension-tool-{}", process::id()));
+    let tmp_dir = std::env::temp_dir().join(format!("gnome-extension-tool-{}", process::id()));
 
     fs::create_dir(tmp_dir.clone())?;
 
@@ -31,98 +28,92 @@ pub fn download(url: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
             .and_then(|name| if name.is_empty() { None } else { Some(name) })
             .unwrap_or("tmp.bin");
 
-        println!("file to download: '{}'", fname);
         let fname = tmp_dir.join(fname);
-        // let fname = tmp_dir.path().join(fname);
-        println!("will be located under: '{:?}'", fname);
         let mut fh = File::create(fname.clone())?;
 
-        let mut content =  Cursor::new(response.bytes()?);
+        let mut content = Cursor::new(response.bytes()?);
         copy(&mut content, &mut fh)?;
-  
+
         fname
     };
 
     Ok(dest)
-
 }
 
-
 /// Install a GNOME extension from url
-pub fn install(url: String) -> Result<(), Box<dyn std::error::Error>> {
-
-    // Parse the url for the pk
-    // https://extensions.gnome.org/extension/4679/burn-my-windows/
-
+pub fn install(url: String) -> Result<String, Box<dyn std::error::Error>> {
     let gnome_shell_version = gnome::get_shell_version()?;
-    println!("Version: {:?}", gnome_shell_version);
-    if 0 < gnome_shell_version {
-
-        let uuid = get_uuid_by_url(url.clone().to_string())?;
-
-        // Search by uuid
-        let results = search::search(uuid.to_string());
-        match results {
-            Ok(extensions) => {
-                let extension = extensions.extensions.into_iter().nth(0).unwrap();
-                println!("extension uuid: {}", extension.uuid);
-                let pk = extension.shell_version_map.get(gnome_shell_version.to_string().as_str()).unwrap().pk;
-                // extension.
-                let download_url = format!(
-                    "https://extensions.gnome.org/download-extension/{}.shell-extension.zip?version_tag={}",
-                    extension.uuid,
-                    pk
-                );
-
-                match download(download_url) {
-                    Ok(dest) => {
-                        println!("Got zip: {:?}", dest);
-
-                        // `gnome-extension install` doesn't seem to work for me.
-                        // Let's bypass it and do it manually.
-                        // Unzip the file to ~/.local/share/gnome-shell/extensions/{uuid}, i.e.
-                        // ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io
-                        install_zip(dest, uuid);
-
-                        // enable_extension(extension.uuid);
-
-                    },
-                    Err(error) => { panic!("Error: {}", error)},
-                }
-            },
-            Err(error) => { panic!("Error searching extensions.gnome.org: {:?}", error) }
-        }
+    if 0 == gnome_shell_version {
+        // return Err(std::fmt::Error("asdf"));
+        panic!("Couldn't determine GNOME shell version.");
     }
 
-    Ok(())
+    let uuid = get_uuid_by_url(&url)?;
+
+    // Search by uuid
+    let results = search::search(uuid.to_string());
+    match results {
+        Ok(extensions) => {
+            let extension = extensions.extensions.into_iter().nth(0).unwrap();
+            let pk = extension
+                .shell_version_map
+                .get(gnome_shell_version.to_string().as_str())
+                .unwrap()
+                .pk;
+            // extension.
+            let download_url = format!(
+                "https://extensions.gnome.org/download-extension/{}.shell-extension.zip?version_tag={}",
+                extension.uuid,
+                pk
+            );
+
+            match download(download_url) {
+                Ok(dest) => {
+                    // Unzip the file to ~/.local/share/gnome-shell/extensions/{uuid}, i.e.
+                    // ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io
+                    match install_zip(dest, &uuid) {
+                        Ok(_ok) => println!("Extension {:?} installed successfully.", uuid),
+                        Err(e) => panic!("Unable to install zip file: {:?}", e),
+                    }
+
+                    // enable_extension(extension.uuid);
+                }
+                Err(error) => {
+                    panic!("Error: {}", error)
+                }
+            }
+        }
+        Err(error) => {
+            panic!("Error searching extensions.gnome.org: {:?}", error)
+        }
+    }
+    Ok(uuid.to_string())
 }
 
 /// Get the extension's UUID from it's homepage
-fn get_uuid_by_url(url: String) -> Result<String, Box<dyn std::error::Error>> {
-    let response = reqwest::blocking::get(url)?;
-    let html = response.text()?;
+fn get_uuid_by_url(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let uuid = {
+        let response = reqwest::blocking::get(url)?;
+        let html = response.text()?;
 
-    let re = Regex::new(
-        "data-uuid=\"([^\"]+)"
-    ).unwrap();
-    let caps = re.captures(html.as_str()).ok_or("Couldn't find UUID")?;
-    let uuid = caps.get(1).unwrap().as_str().to_string();
+        let re = Regex::new("data-uuid=\"([^\"]+)").unwrap();
+        let caps = re.captures(html.as_str()).ok_or("Couldn't find UUID")?;
+        caps.get(1).unwrap().as_str().to_owned()
+    };
 
-    Ok(uuid)
+    Ok(uuid.clone().to_string())
 }
 
 /// Install an extension via zip file
-fn install_zip(path: PathBuf, uuid: String) -> Result<(), Box<dyn std::error::Error>> {
-
+fn install_zip(path: PathBuf, uuid: &str) -> Result<(), Box<dyn std::error::Error>> {
     // unzip the file to ~/.local/share/gnome-shell/extensions/<uuid>
     let file = fs::File::open(path).unwrap();
 
-    println!("File: {:?}", file);
     let mut archive = zip::ZipArchive::new(file).unwrap();
 
     for i in 0..archive.len() {
         let mut file = archive.by_index(i).unwrap();
-        // Build the path to 
+        // Build the path to
         let outpath = match file.enclosed_name() {
             Some(path) => Path::new(&home_dir().unwrap())
                 .join(Path::new(".local/share/gnome-shell/extensions"))
@@ -132,15 +123,8 @@ fn install_zip(path: PathBuf, uuid: String) -> Result<(), Box<dyn std::error::Er
         };
 
         if (*file.name()).ends_with('/') {
-            println!("File {} extracted to \"{}\"", i, outpath.display());
             fs::create_dir_all(&outpath).unwrap();
         } else {
-            // println!(
-            //     "File {} extracted to \"{}\" ({} bytes)",
-            //     i,
-            //     outpath.display(),
-            //     file.size()
-            // );
             if let Some(p) = outpath.parent() {
                 if !p.exists() {
                     fs::create_dir_all(p).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod gnome;
 mod install;
 mod search;
 
-use clap::{Parser, Subcommand};
+use std::process::ExitCode;
 
+use clap::{Parser, Subcommand};
+use url::Url;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -17,9 +19,8 @@ struct Args {
     verbose: bool,
 
     /// The URL to the extension on extensions.gnome.org
-    url: String
+    url: String,
 }
-
 
 #[derive(Subcommand)]
 enum Commands {
@@ -27,18 +28,33 @@ enum Commands {
     Install { url: String },
 
     /// Search extensions by keyword
-    Search { keywords: String, shell: Option<i32> },
+    Search {
+        keywords: String,
+        shell: Option<i32>,
+    },
 }
 
-fn main() {
+fn main() -> ExitCode {
     let args = Args::parse();
 
     // TODO: validate that the url is well-formed
+    let url = match Url::parse(args.url.as_str()) {
+        Ok(url) => url,
+        Err(_e) => {
+            if !args.quiet {
+                println!("Unable to parse URL.");
+            }
+            return ExitCode::FAILURE;
+        }
+    };
 
-    match install::install(args.url.to_string()) {
-        Ok(success) => { 
-            println!("Got body: {:?}", success);
-        },
-        Err(error) => { panic!("Error: {}", error)},
-    }
+    match install::install(url.to_string()) {
+        Ok(_uuid) => {
+            if !args.quiet {
+                println!("Please restart your gnome-session.");
+            }
+            return ExitCode::SUCCESS;
+        }
+        Err(_error) => return ExitCode::FAILURE,
+    };
 }
